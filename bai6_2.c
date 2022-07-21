d80121193e19e85a1a529512121105e7c81f4e9e
Tbthanh <thanhtb2003ltv@gmail.com>
2022-07-22 01:00:34 +0700

    f code

---
+++ bai6_2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+/*
+testcase: "          H          :  i            "
+expected: "H : i"
+*/
+int main()
+{
+    char s[100]={0};
+    fgets(s,100,stdin);
+    
+    int i,lens,j;
+
+    lens = strlen(s);
+
+    //xu ly dau dong
+    for (i = 0; i < lens; i++)
+    {
+        if(s[i]!=32)
+        {
+            j=i;
+            break;
+        }
+    }
+    for (i=0; i<lens;i++)
+    {
+        s[i] = s[i+j];
+        s[i+j] = 0;
+    }
+    lens-=j;
+    
+    //xu ly cuoi cau
+    for (i = lens-1; i >= 0; i--)
+    {
+        if(s[i]!=32)
+        {
+            j=i;
+            break;
+        }
+        else
+        {
+            s[i]=0;
+        }
+    }
+    lens= j;
+    //xu ly trong cau
+    for(i=0;i<lens;i++)
+    {
+        if(s[i]==32)
+        {
+            //haha
+        }
+    }
+
+    puts(s);
+    return 0;
+}
